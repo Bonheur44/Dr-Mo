c70e229c3c93eebb7cdc40911ec26adcb5005c1f
Bonheur44 <zachariemeyevi@gmail.com>
2022-03-02 21:47:50 +0100

    mise à jour

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,34 +1,12 @@
-let byFile = document.querySelector('#byFile')
-let byField = document.querySelector('#byField')
 let text = document.querySelector('#text')
-let file = document.querySelector('#file')
 let p = document.querySelector('#words')
 let lauchCount = document.querySelector('#lauchCount')
 
 let regexp = /[a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ-]/
 
-byField.addEventListener('change', function () {
-    file.setAttribute('disabled', 'disabled')
-    if (text.getAttribute('disabled') === 'disabled') text.removeAttribute('disabled')
-})
-
-byFile.addEventListener('change', function () {
-    text.setAttribute('disabled', 'disabled')
-    if (file.getAttribute('disabled') === 'disabled') file.removeAttribute('disabled')
-})
-
-file.addEventListener('change', function (event) {
-	const fileList = event.target.files
-	if (fileList[0].type !== 'text/plain') {
-		alert('Le fichier que vous avez sélectionné n\'est pas un fichier txt')
-		lauchCount.setAttribute('disabled', 'disabled')
-	} else lauchCount.removeAttribute('disabled')
-})
-
 lauchCount.addEventListener('click', function (e) {
 	e.preventDefault()
-	if (text.getAttribute('disabled') !== 'disabled') show(count(text.value))
-	else if (file.getAttribute('disabled') !== 'disabled') loadFile(file.files[0])
+	show(count(text.value))
 })
 
 let count = (text) => {
@@ -64,10 +42,6 @@ let count = (text) => {
 		i++
 	} while (i < l)
 	return nbrMots
-}
-
-async function loadFile(file) {
-	show(count(await file.text()))
 }
 
 function show(num) {
